a732a0467e6830a0eafe90d73aff30fcf6293d5f
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-04-13 13:25:33 +0200

    1304

--- essai/app.js
+++ essai/app.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./fire.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Vive des vacances!')
 })
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
