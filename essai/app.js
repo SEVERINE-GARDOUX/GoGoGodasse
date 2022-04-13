d08c4b65dc8d7cdd291eccd153f5a7c15ca6f9e7
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-04-13 14:50:35 +0200

    1304

--- essai/app.js
+++ essai/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const { getFirestore } = require('firebase-admin/firestore');
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./fire.json");
@@ -10,6 +10,15 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const db = getFirestore();
+
+async function add() {
+    const res = await db.collection('cities').add({
+        name: 'Tokyo',
+        country: 'Japan'
+})
+}
+add()
 
 app.get('/', (req, res) => {
   res.send('Vive des vacances!')
