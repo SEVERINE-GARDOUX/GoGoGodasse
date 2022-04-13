2ec720854cfcb97b70affcad43edb007022ea38e
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-04-13 11:25:26 +0200

    1304

--- firebase/main.js
+++ firebase/main.js
@@ -1,3 +1,5 @@
+// import (easyFire) from 'https://cdn.jsdelivr.net/gh/DocteurSEO/easyfire@master/easyfirev0.5.1.js'
+import {creatHTML, getHTML, addClick, getParam} from 'https://cdn.jsdelivr.net/gh/DocteurSEO/easyfire@master/easyjsv0.0.9.js'
 const firebaseConfig = {
     apiKey: "AIzaSyAWK0Og9XqOKAfgt2NtIJb5_HkFl5-3zvo",
     authDomain: "first-projet-d093d.firebaseapp.com",
