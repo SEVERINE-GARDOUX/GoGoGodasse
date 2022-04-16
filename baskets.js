853c8e0e35de446982606b69373a53bbb92d8e09
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-04-16 17:15:11 +0200

    1604

---
+++ baskets.js
@@ -0,0 +1,9 @@
+const refImage = getDownloadUrl (dbfirebase)
+
+// function click refImage 
+//au clic ça va chercher les infos dans la db
+// affiche dans page produit le nom et l'image
+
+
+//function click add sur le button ajouter de la page produit
+//affiche la photo le nom et la pointure dans la page panier
